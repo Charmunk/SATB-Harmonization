952e549c88166b085b3fabba4058ef2a86baa63c
madeleine <128001078+madeleieine@users.noreply.github.com>
2026-05-07 14:19:34 -0700

    working on chord stuff dict is good yay

--- mm_part_to_part.py
+++ mm_part_to_part.py
@@ -21,9 +21,9 @@ def csv_to_tracks(file_path):
         file_path, 
         delimiter=',', 
         unpack=True, 
-        skip_header=1
+        skip_header=1,
+        dtype=int
     )
-    
     return S, A, T, B
 
 
